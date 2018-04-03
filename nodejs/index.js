71c4a450df15925476e66c897a02df934fe17d23
tinhnguyen <tinh.nguyen@exnodes.vn>
2018-04-03 18:25:48 +0700

    Add user online and click to chat

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,9 +1,24 @@
-const app = require('./app.js');
+var app = require('./app.js');
+// Socket io
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var userOnline = [];
+
+io.on('connection', function(socket) {
+   console.log('connection');
+   socket.on('client-send-peer-id', function(data) {
+       userOnline.push(data.peerId);
+       // if (userOnline.length > 2) userOnline = [];
+       console.log(data);
+      io.sockets.emit('server-send-user-online', {userOnline: userOnline});
+   })
+});
 
 
 
 
 
-app.listen(4000, () => {
-   console.log('Start!');
+server.listen(4000, () => {
+    console.log('Start!');
 });
--- reactjs/js/main.js
+++ reactjs/js/main.js
@@ -1,3 +1,21 @@
+// Socket.io
+var socket = io.connect('http://localhost:4000');
+
+function clientSendPeerId(peerId) {
+    socket.emit('client-send-peer-id', {peerId: peerId});
+}
+
+socket.on('server-send-user-online', function(data) {
+    console.log(data);
+    $('#userOnline').html('');
+    data.userOnline.map(function(val){
+        $('#userOnline').append(`<li id="${val}">${val}</li>`);
+    });
+    call();
+});
+
+
+// Display video
 getUserMedia('#my-stream', '#errorMsg');
 
 // var peer = new Peer({key: 'n75dq11v0jk2zkt9'});
@@ -10,31 +28,40 @@ var peer = new Peer({
 });
 
 // Create peer
-peer.on("open", id => {
-    $("#my-peer-id").html('My ID: ' + id);
-    console.log(id);
+peer.on("open", peerId => {
+    $("#my-peer-id").html('My ID: ' + peerId);
+    // Send peer id o server
+    clientSendPeerId(peerId);
+    console.log(peerId);
 });
 
-// Caller
-$('#other-peer-id').submit(function(){
-    var orderPeerId = $(this).find('input[type=text]').val();
-    console.log(orderPeerId);
-    // Get stream
-    getUserMedia('#my-stream', '#errorMsg', function (stream) {
-        // Call a peer, providing our mediaStream
-        var call = peer.call(orderPeerId, stream);
-        console.log(call);
-        call.on('stream', function(remoteStream) {
-            var video = document.querySelector('#other-stream');
-            video.srcObject = remoteStream;
+
+
+
+
+function call() {
+    $("#userOnline li").on('click', function(){
+        $('#other-stream').show();
+        // Caller
+        var orderPeerId = $(this).attr('id');
+        // Get stream
+        getUserMedia('#my-stream', '#errorMsg', function (stream) {
+            // Call a peer, providing our mediaStream
+            let call = peer.call(orderPeerId, stream);
+            call.on('stream', function(remoteStream) {
+                let video = document.querySelector('#other-stream');
+                video.srcObject = remoteStream;
+            });
         });
-    });
 
-    return false;
-});
+        return false;
+    });
+}
+
 
 // Answer
 peer.on('call', function(call) {
+    $('#other-stream').show();
     // Get stream
     getUserMedia('#my-stream', '#errorMsg', function(stream) {
         call.answer(stream);
@@ -44,3 +71,7 @@ peer.on('call', function(call) {
         });
     });
 });
+
+
+
+
