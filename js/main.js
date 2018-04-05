fcb9a23312672a1a0a2ee18cd122862b8b6f0e2e
tinhnguyen <tinh.nguyen@exnodes.vn>
2018-04-05 15:05:18 +0700

    Add xirsys and deferred function

--- js/main.js
+++ js/main.js
@@ -1,63 +1,51 @@
+// Call Xirsys server
 var myIceServers;
+var socket;
+var peer;
 
-$( document ).ready( function () {
-    $.ajax ({
-        url: "https://global.xirsys.net/_turn/nttinh86.github.io/",
-        type: "PUT",
-        async: false,
-        headers: {
-            "Authorization": "Basic " + btoa("nttinh86:156d7bc6-27f7-11e8-b1b7-9a80c18987a0")
-        },
-        success: function (res){
-            console.log(res.v.iceServers);
-            myIceServers = res.v.iceServers;
-        }
+$.ajax ({
+    url: "https://global.xirsys.net/_turn/nttinh86.github.io/",
+    type: "PUT",
+    async: false,
+    headers: {
+        "Authorization": "Basic " + btoa("nttinh86:156d7bc6-27f7-11e8-b1b7-9a80c18987a0")
+    }
+}).then(function(res){
+    myIceServers = res.v.iceServers;
+
+    // Socket.io
+    socket = io.connect('https://nttinh86nodejs.herokuapp.com');
+
+    // Create peer
+    peer = new Peer({
+        host: 'nttinh86peerserver.herokuapp.com',
+        secure: true,
+        port: 443,
+        key: 'peerjs',
+        debug: 3,
+        config: myIceServers
     });
-});
 
-// Socket.io
-var socket = io.connect('https://nttinh86nodejs.herokuapp.com');
-
-function clientSendPeerId(peerId) {
-    socket.emit('client-send-peer-id', {peerId: peerId});
-}
-
-socket.on('server-send-user-online', function(data) {
-    console.log(data);
-    $('#userOnline').html('');
-    data.userOnline.map(function(val){
-        $('#userOnline').append(`<li id="${val}">${val}</li>`);
+    peer.on("open", peerId => {
+        $("#my-peer-id").html('My ID: ' + peerId);
+        // Send peer id o server
+        socket.emit('client-send-peer-id', {peerId: peerId});
     });
-    call();
-});
 
+    var def = $.Deferred();
+    socket.on('server-send-user-online', function(data) {
+        $('#userOnline').html('');
+        data.userOnline.map(function(val){
+            $('#userOnline').append(`<li id="${val}">${val}</li>`);
+        });
+        def.resolve();
+    });
+    return def.promise();
 
-// Display video
-getUserMedia({videoTag: '#my-stream', volume: 0}, '#errorMsg');
+}).then(function() {
+    // Display video
+    getUserMedia({videoTag: '#my-stream', volume: 0}, '#errorMsg');
 
-// var peer = new Peer({key: 'n75dq11v0jk2zkt9'});
-var peer = new Peer({
-    host: 'nttinh86peerserver.herokuapp.com',
-    secure: true,
-    port: 443,
-    key: 'peerjs',
-    debug: 3,
-    config: myIceServers
-});
-
-// Create peer
-peer.on("open", peerId => {
-    $("#my-peer-id").html('My ID: ' + peerId);
-    // Send peer id o server
-    clientSendPeerId(peerId);
-    console.log(peerId);
-});
-
-
-
-
-
-function call() {
     $("#userOnline li").on('click', function(){
         $('#other-stream').show();
         // Caller
@@ -74,22 +62,18 @@ function call() {
 
         return false;
     });
-}
 
-
-// Answer
-peer.on('call', function(call) {
-    $('#other-stream').show();
-    // Get stream
-    getUserMedia({videoTag: "#my-stream", volume: 0}, '#errorMsg', function(stream) {
-        call.answer(stream);
-        call.on('stream', function(remoteStream){
-            var video = document.querySelector('#other-stream');
-            video.srcObject = remoteStream;
+    // Answer
+    peer.on('call', function(call) {
+        $('#other-stream').show();
+        // Get stream
+        getUserMedia({videoTag: "#my-stream", volume: 0}, '#errorMsg', function(stream) {
+            call.answer(stream);
+            call.on('stream', function(remoteStream){
+                var video = document.querySelector('#other-stream');
+                video.srcObject = remoteStream;
+            });
         });
     });
+
 });
-
-
-
-
