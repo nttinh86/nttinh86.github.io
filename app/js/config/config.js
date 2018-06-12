8cd55ef37c92813f80b5101329b4909ec45883c6
tinh nguyen <nttinh86@gmail.com>
2018-06-12 09:55:02 +0700

    3000

--- app/js/config/config.js
+++ app/js/config/config.js
@@ -1,4 +1,4 @@
 // const SOCKET_SERVER_URL = "https://nttinh86nodejs.herokuapp.com";
-const SOCKET_SERVER_URL = "https://nttinh86backend.herokuapp.com:4000";
+const SOCKET_SERVER_URL = "https://nttinh86backend.herokuapp.com";
 
 export {SOCKET_SERVER_URL}
